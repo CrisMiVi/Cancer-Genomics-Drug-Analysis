cbd683113d31448d73203570bcde3aed12d31b80
Cris <cristinamiguelvijandi@gmail.com>
2026-03-25 19:42:18 +0000

    Update 01_cleaning_scripts.sql.v3

--- sql/01_cleaning_scripts.sql
+++ sql/01_cleaning_scripts.sql
@@ -74,15 +74,15 @@ DATE: March 2026
 
 -- 2A. PK: COSMIC_ID and DRUG_ID
   
-    SELECT
-      COSMIC_ID
-      , DRUG_ID
-      , COUNT(*) AS row_count
-    FROM GDSC2
-    GROUP BY 
-      COSMIC_ID
-      , DRUG_ID
-    HAVING row_count > 1 
+  SELECT
+    COSMIC_ID
+    , DRUG_ID
+    , COUNT(*) AS row_count
+  FROM GDSC2
+  GROUP BY 
+    COSMIC_ID
+    , DRUG_ID
+  HAVING row_count > 1 
 
 -- 3B. Nulls values: 
     /* 
@@ -90,14 +90,14 @@ DATE: March 2026
        - TCGA_DESC and PUTATIVE_TARGET had null values. They were replaced with "unkown".
     */
   ---> TCGA_DESC has 1067/ 242036 null values, substitute them with 'unkown'.
-    SELECT COUNT(*)
-    FROM GDSC2
-    WHERE TCGA_DESC IS NULL 
+  SELECT COUNT(*)
+  FROM GDSC2
+  WHERE TCGA_DESC IS NULL 
 
   ---> PUTATIVE_TARGET has 27155/ 242036 null values, substitute them with 'unkown'.
-    SELECT COUNT(*)
-    FROM GDSC2
-    WHERE PUTATIVE_TARGET IS NULL 
+  SELECT COUNT(*)
+  FROM GDSC2
+  WHERE PUTATIVE_TARGET IS NULL 
 
 -- 3C. Data filtering:
     /* 
@@ -147,7 +147,103 @@ DATE: March 2026
           WHEN Z_SCORE > 0.5 OR LN_IC50 > LOG(MAX_CONC) THEN 'RESISTANT'
           ELSE 'INTERMEDIATE'
       END AS SENSITIVITY_CALL
-    FROM GDSC2
+    FROM GDSC2 
+
+
+-----------------------------------------------------------------------------------------------
+--- 4. TABLE: TGCA_tissue_classification ---
+-----------------------------------------------------------------------------------------------
+
+-- 4A. Add labels to the metrics
+  SELECT
+  string_field_0 AS TCGA_DESC
+  , string_field_1 AS TCGA_name
+  FROM TGCA_tissue_classification
+      
+
+-----------------------------------------------------------------------------------------------
+--- 5. TABLE: SCREENED_COMPOUNDS ---
+-----------------------------------------------------------------------------------------------
+
+-- 5A. PK: DRUG_ID
+  SELECT 
+    DRUG_ID
+    , COUNT(*) AS drug_ID_count
+  FROM `tranquil-gasket-457509-b3.CGDA.screened_compounds` 
+  GROUP BY DRUG_ID
+  HAVING drug_ID_count > 1; 
+
+-- 5B. Blank values: 
+    /*    
+      - There are no null values, but there are blank values. 
+      - Blank values: 43 blank values in TARGET and 201 blank values for SYNONYMS. 
+      - Will substitute TARGET blank values with "unkown" and ignore SYNONYMS because they are too many and this field is not relevant.
+      - This changes will be applied to the next cleaning step.
+    */
+
+-- 5C. Duplicated values: 
+  /*
+Entity Resolution & Drug Metadata Normalization
+In the initial dataset, a many-to-many relationship was identified between DRUG_ID and DRUG_NAME (621 unique names across 542 unique IDs). To ensure each therapeutic agent was represented by a single, high-quality primary key for downstream analysis, I implemented a multi-stage deduplication pipeline:
+
+Step 1: Assay-Based Filtering: I cross-referenced the compound library against the active GDSC2 screening results. This narrowed the scope to only those drugs with empirical response data, reducing the number of duplicate name-ID pairs from 71 to 9.
+
+Step 2: Quality-Driven Selection: For the remaining 9 ambiguous cases, I developed a selection hierarchy to identify the "Golden Record" for each drug name. I prioritized IDs based on:
+
+Data Reliability: Selecting the ID with the lowest average RMSE (highest curve-fit quality).
+
+Statistical Power: Selecting the ID with the highest Experimental Count (total number of screens).
+
+Step 3: Database Normalization: Using this filtered list of unique DRUG_ID and DRUG_NAME pairs, I generated a cleaned reference table (screened_compounds_clean). This ensured that all subsequent joins remained 1-to-1, preventing data inflation and ensuring consistent drug labeling across the entire study.
+
+*/
+
+  # B1. ---> When looking for unique drug_names I found that there are different DRUG_IDs with the same name (71 duplicated drug_names). There are 542 unique DRUD_ID and 621 unique DRUG_NAME, siggesting that the same drug_names can be associated to multiple DRUG_ID. 
+  
+  SELECT 
+    DRUG_NAME
+    , COUNT(*) AS ROW_COUNT 
+  FROM `tranquil-gasket-457509-b3.CGDA.screened_compounds` 
+  GROUP BY DRUG_NAME 
+  HAVING ROW_COUNT > 1
+  
+  /* To keep onky one DRUG_NAME per DRUG_ID I will do the following:
+    1. Join the table screened_compounds to GDSC2_cleaned so that I keep only the drugs that have been used in the screening assays. This narrows down from 71 DRUG_ID with multiple DRUG_NAME to 9. 
+    2. For each DRUG_NAME, order them by lowest average RMSE (more reliable data) and highest nunmber of screens (more data points) reated to the GDSC2_cleaned table. And selected the one with lowest RMSE and highest experimental count. This eliminated the duplicated DRUG_NAME from the remaining 9 DRUG_ID with multiple DRUG_NAMES. 
+    3. Use the list of unique_DRUG_ID and DRUG_NAME to clean the table screened_compounds and create the table screened_compounds_clean. 
+    */ 
+
+  WITH drug_ranking AS (
+    SELECT 
+        DRUG_NAME
+        ,DRUG_ID
+        -- Assign row number based on: Lowest RMSE, then most data points
+        ,ROW_NUMBER() OVER(
+            PARTITION BY DRUG_NAME 
+            ORDER BY AVG(RMSE) ASC, COUNT(*) DESC
+        ) as preference_rank
+    FROM `tranquil-gasket-457509-b3.CGDA.GDSC2_cleaned`
+    GROUP BY DRUG_NAME, DRUG_ID
+  ),
+  -- This list will have the 286 unique drug_IDs, select only rows number 1 with lowest RMSE and most data points for each DRUG_ID 
+  best_ranked_table AS(
+    SELECT 
+      DRUG_ID
+  FROM drug_ranking 
+  WHERE preference_rank = 1
+  )
+  -- Select the cleaned variables for table screened_compounds
+  SELECT
+    comp.DRUG_ID
+    , SCREENING_SITE
+    , DRUG_NAME
+    , CASE WHEN `TARGET` = "" THEN "unknown" ELSE `TARGET` END AS `TARGET`
+    , TARGET_PATHWAY
+  FROM tranquil-gasket-457509-b3.CGDA.screened_compounds AS comp
+  JOIN best_ranked_table AS ranked
+  ON comp.DRUG_ID = ranked.DRUG_ID
+  -- This table was saved as screened_compounds_cleaned
+
    
   
 
