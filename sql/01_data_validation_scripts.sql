c52948348d8e0d4b6941f16a880e6f627b1f4877
Cris <cristinamiguelvijandi@gmail.com>
2026-03-30 16:41:07 +0100

    Update and rename 01_validation_scripts.sql

--- sql/01_data_validation_scripts.sql
+++ sql/01_data_validation_scripts.sql
@@ -1,5 +1,5 @@
 /* PROJECT: Cancer Genomics Drug Analysis (GDSC)
-SCRIPT: 01_validation_scripts.sql
+SCRIPT: 01_data_validation_scripts.sql
 PURPOSE: Identifying structural inconsistencies, PK violations, and null distributions.
 DATE: March 2026
 */
@@ -11,22 +11,22 @@ DATE: March 2026
 
 -- AUDIT: COSMIC_tissue_classification PK
 SELECT COSMIC_ID, COUNT(*) AS row_count
-FROM `PROJECT_ID.CGDA.COSMIC_tissue_classification`
+FROM `CGDA.COSMIC_tissue_classification`
 GROUP BY 1 HAVING row_count > 1;
 
 -- AUDIT: Cell_line_details PK
 SELECT COSMIC_identifier, COUNT(*) AS row_count
-FROM `PROJECT_ID.CGDA.Cell_line_details`
+FROM `CGDA.Cell_line_details`
 GROUP BY 1 HAVING row_count > 1;
 
 -- AUDIT: GDSC2 Composite PK (Cell + Drug)
 SELECT COSMIC_ID, DRUG_ID, COUNT(*) AS row_count
-FROM `PROJECT_ID.CGDA.GDSC2`
+FROM `CGDA.GDSC2`
 GROUP BY 1, 2 HAVING row_count > 1;
 
 -- AUDIT: screened_compounds PK
 SELECT DRUG_ID, COUNT(*) AS row_count
-FROM `PROJECT_ID.CGDA.screened_compounds`
+FROM `CGDA.screened_compounds`
 GROUP BY 1 HAVING row_count > 1;
 
 ---------------------------------------------------------------------------------
@@ -36,17 +36,17 @@ GROUP BY 1 HAVING row_count > 1;
 
 -- AUDIT: Nulls in Cell Line Metadata
 SELECT 
-    COUNTIF(`Cancer Type_TCGA` IS NULL) AS null_tcga,
-    COUNTIF(Microsatellite_instability_Status_MSI IS NULL) AS null_msi
-FROM `PROJECT_ID.CGDA.Cell_line_details`;
+    COUNTIF(`Cancer Type_TCGA` IS NULL) AS null_tcga
+    , COUNTIF(Microsatellite_instability_Status_MSI IS NULL) AS null_msi
+FROM `CGDA.Cell_line_details`;
 
 -- AUDIT: Nulls in Drug Response (GDSC2)
 SELECT 
-    COUNTIF(TCGA_DESC IS NULL) AS null_tcga_desc,
-    COUNTIF(PUTATIVE_TARGET IS NULL) AS null_target
-FROM `PROJECT_ID.CGDA.GDSC2`;
+    COUNTIF(TCGA_DESC IS NULL) AS null_tcga_desc
+    , COUNTIF(PUTATIVE_TARGET IS NULL) AS null_target
+FROM `CGDA.GDSC2`;
 
 -- AUDIT: Blank strings in Drug Targets
 SELECT COUNT(*) AS blank_targets
-FROM `PROJECT_ID.CGDA.screened_compounds`
+FROM `CGDA.screened_compounds`
 WHERE TRIM(`TARGET`) = "";
